e0a592617ad9de4d8f1f91aac53a40c3d98a5b0b
mary-soroka <49789721+mary-soroka@users.noreply.github.com>
2021-02-21 13:24:23 -0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+/* MUSIC */
+
 document.getElementById('ItsLove').onclick = function() {
   document.getElementById('its-love').play();
 };
@@ -18,3 +20,34 @@ document.getElementById('stopButton').onclick = function() {
   var sounds = document.getElementsByTagName('audio');
   for(i=0; i<sounds.length; i++) sounds[i].pause();
 };
+
+
+/* MSN */
+
+document.getElementById('msn').onclick = function() {
+  document.getElementById('beep').play();
+};
+
+/* BEEPS 
+
+document.getElementById('ItsLove').onmouseover = function() {
+	document.getElementById('beep').play();
+};
+
+document.getElementById('MyMind').onmouseover = function() {
+	document.getElementById('beep').play();
+};
+
+document.getElementById('Song3').onmouseover = function() {
+	document.getElementById('beep').play();
+};
+
+document.getElementById('Song4').onmouseover = function() {
+	document.getElementById('beep').play();
+};
+
+document.getElementById('stopButton').onmouseover = function() {
+	document.getElementById('beep').play();
+};
+
+*/
